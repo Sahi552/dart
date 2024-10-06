55a7e84a34debd4e2ed8a4c929ecd53268544428
sahit <sahithiyani280@gmail.com>
2024-10-06 11:22:56 +0530

    comitted on 6/10

--- demo.dart
+++ demo.dart
@@ -1,4 +1,17 @@
-main() {
-  String Name = "Sahithiyan";
+import 'dart:io';
+
+void main() {
+  var nickname = 'agent';
+  int age = 22;
+  String Name = 'Sahithiyan';
+  const String anime = 'naruto shippuden';
+  final String character = 'naruto';
   print(Name);
+  print(age);
+  print(nickname + "g");
+  print(anime);
+  print(character);
+  print(
+      "my name is $Name and my age is $age and i love anime namely $anime named after $character"
+      );
 }
---
+++ list.dart
@@ -0,0 +1,16 @@
+void main() {
+  var anime = ['naruto', 'black clover', 'one piece'];
+  print(anime);
+
+  print(anime[2]);
+
+  var empty = [];
+  print(empty);
+
+  empty.add(29);
+  print(empty);
+  empty.add(0);
+
+  empty.insert(2, 3);
+  print(empty);
+}
