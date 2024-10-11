cb1d3057e8f674af1ceb0dd33ec45d3f6c576bc5
sahit <sahithiyani280@gmail.com>
2024-10-11 20:07:42 +0530

    commmited on 11/10

--- classondobj.dart
+++ classondobj.dart
@@ -23,4 +23,22 @@ void main() {
   var p1 = Person('sahi', 21);
   p1.printinfo();
   p1.voterights();
+  var obj1 = pc('god of war');
+  obj1. summa();
+  var obj2 = pc('ARK');
+  obj2.summa();
+}
+
+abstract class Games {
+  String Game;
+  Games(this.Game);
+  void summa();
+}
+
+class pc extends Games{
+  pc(super.Game);
+  @override
+  void summa(){
+    print('$Game');
+  }
 }
